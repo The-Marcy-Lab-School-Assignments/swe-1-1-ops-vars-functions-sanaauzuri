99696ff066058e7e3ceecb6006934d2da16b42c8
Sanaa-Uzuri Robotham <sanaauzurirm@gmail.com>
2025-10-07 19:32:29 -0400

    From Scratch Questions Complete

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,35 +1,51 @@
 // Basic Variable & Expression Functions
 const calculateArea = (width, height) => {
+  return width * height
   // TODO: Calculate and return the area of a rectangle
 };
 
 const isEven = (number) => {
+  if (number % 2 === 0) {
+    return true
+  } else {
+    return false
+  }
   // TODO: Return true if the number is even, false if odd
 };
 
 const convertToFahrenheit = (celsius) => {
+  return (celsius * 9/5) + 32
   // TODO: Convert celsius to fahrenheit using the formula: (celsius * 9/5) + 32
-};
+}
 
 // String & Variable Functions
 const createGreeting = (name) => {
+  return `Hello, ${name}!`
   // TODO: Return a greeting string like "Hello, [name]!"
 };
 
 const getInitials = (firstName, lastName) => {
+  return firstName.charAt(0) + lastName.charAt(0)
   // TODO: Return initials by taking first letter of each name
 };
 
 const formatPrice = (price) => {
+  return `$${price}.00`
   // TODO: Return price formatted as "$[price].00" (e.g., "$5.00")
 };
 
 // Function Logic Functions
 const getLarger = (num1, num2) => {
+  return Math.max(num1, num2)
   // TODO: Return the larger of the two numbers
 };
 
 const isValidAge = (age) => {
+  if (age <= 120 && age >= 0) {
+    return true
+  } else {
+    return false
+  }
   // TODO: Return true if age is between 0 and 120, false otherwise
 };
 
