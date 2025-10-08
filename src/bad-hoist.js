d137a563c87dcf4e4dfb58ab6db9fe261f9c6ab9
Sanaa-Uzuri Robotham <sanaauzurirm@gmail.com>
2025-10-07 20:01:56 -0400

    Question 11

--- src/bad-hoist.js
+++ src/bad-hoist.js
@@ -1,11 +1,12 @@
 const debugFunc = () => {
-  time = 'today';
-  console.log('Hello ' + name + ", are you feeling " + mood + ' ' + time + '?');
-  var name = 'Zo';
-  var mood = 'happy';
-
-  console.log("Oh no, I'm sorry you're feeling " + mood + ' ' + time + '.');
-  mood = 'sad';
+  let name = 'Zo';
+  let mood = 'happy';
+  let time = 'today';
+  console.log(`Hello ${name}, are you feeling ${mood} ${time}?`);
+  
+  
+  let newMood = 'sad';
+  console.log(`Oh no, I'm sorry you're feeling ${newMood} ${time}.`);
 };
 
 debugFunc();
